b697e1d2d1e69409448b4d32c3d26851dae9f612
dex22044 <dex22044@yandex.ru>
2020-03-29 18:17:12 +0300

    окно просмотра(или нет?)

---
+++ SmallBusiness/SmallBusiness/ViewWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmallBusiness
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewWindow.xaml
+    /// </summary>
+    public partial class ViewWindow : Window
+    {
+        public ViewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddProd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteProd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditProd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackBtnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
