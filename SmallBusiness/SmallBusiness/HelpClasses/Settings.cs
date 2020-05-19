8563f396e8e366e51119fb2641d0e093103bce96
dex22044 <dex22044@yandex.ru>
2020-05-19 17:58:59 +0300

    запоминание данных входа

--- SmallBusiness/SmallBusiness/HelpClasses/Settings.cs
+++ SmallBusiness/SmallBusiness/HelpClasses/Settings.cs
@@ -9,22 +9,27 @@ namespace SmallBusiness.HelpClasses
 {
     class Settings
     {
-        public static void SaveAccount (string login, string password)
+        public static void SaveAccount (string login, string password, bool isChecked)
         {
             RegistryKey CurrentUser = Registry.CurrentUser;
             RegistryKey SB = CurrentUser.CreateSubKey("SmallBusiness");
             SB.SetValue("login", login);
             SB.SetValue("password", password);
+            SB.SetValue("isAutoLogin", Convert.ToString(isChecked));
             SB.Close();
         }
 
-        public static void LoadAccount (ref string login, ref string password)
+        public static void LoadAccount (ref string login, ref string password, ref bool isChecked)
         {
             RegistryKey CurrentUser = Registry.CurrentUser;
             RegistryKey SB = CurrentUser.OpenSubKey("SmallBusiness");
-            login = SB.GetValue("login").ToString();
-            password = SB.GetValue("password").ToString();
-            SB.Close();
+            if (SB != null)
+            {
+                login = SB.GetValue("login").ToString();
+                password = SB.GetValue("password").ToString();
+                isChecked = Convert.ToBoolean(SB.GetValue("isAutoLogin").ToString());
+                SB.Close();
+            }
         }
     }
 }
