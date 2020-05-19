bdfe09a9902a5f50c6722d0e3edf25a837719e46
lakilev <lev.cat.lev@mail.ru>
2020-05-19 17:58:29 +0300

    добавил кто делал

---
+++ SmallBusiness/SmallBusiness/Forms/SubForms/AbautProgramm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmallBusiness.Forms.SubForms
+{
+    /// <summary>
+    /// Логика взаимодействия для AbautProgramm.xaml
+    /// </summary>
+    public partial class AbautProgramm : UserControl
+    {
+        public AbautProgramm()
+        {
+            InitializeComponent();
+        }
+    }
+}
